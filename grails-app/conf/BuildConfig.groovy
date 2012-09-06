2edf4e03421e0ea78b79d6a97995524d3e3450c2
Rob Meidal <rob@netapplogic.com>
2012-09-06 10:24:40 -0600

    Updated BuildConfig.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,20 +13,12 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+    dependencies{}
+    plugins{
+      compile ":falcone-util:1.1-DEV"
+      compile ":multi-tenant-core:1.1-DEV"
+      compile ":dynamic-controller:0.2.1"
     }
 
 }
